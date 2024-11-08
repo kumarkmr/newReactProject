c2990a86c5d43a45e3f70da3b7d5528ec52e2b04
kumarkmr <naalikumarnaali.com>
2024-11-08 10:25:42 +0530

    Arrow functional code added

---
+++ src/Componets/Charan.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Charan = () => {
+  return <div>Charan</div>;
+};
+
+export default Charan;
---
+++ src/Componets/Praveen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Praveen = () => {
+  return <div>Praveen</div>;
+};
+
+export default Praveen;
